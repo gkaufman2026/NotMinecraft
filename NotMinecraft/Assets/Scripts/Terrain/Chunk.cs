95f8cd44256ce91b430696909b3e15eafc7f962f
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2024-10-29 20:08:21 -0400

    Cleaning up code

--- NotMinecraft/Assets/Scripts/Terrain/Chunk.cs
+++ NotMinecraft/Assets/Scripts/Terrain/Chunk.cs
@@ -2,9 +2,9 @@ using System;
 using UnityEngine;
 
 public static class Chunk {
-    public static void LoopThroughTheBlocks(ChunkData chunkData, Action<int, int, int> actionToPerform) {
+    public static void LoopThroughBlocks(ChunkData chunkData, Action<int, int, int> actionToPerform) {
         for (int i = 0; i < chunkData.blocks.Length; i++) {
-            var pos = GetPostitionFromIndex(chunkData, i);
+            Vector3Int pos = GetPostitionFromIndex(chunkData, i);
             actionToPerform(pos.x, pos.y, pos.z);
         }
     }
@@ -17,7 +17,7 @@ public static class Chunk {
         };
     }
 
-    private static bool InRange(ChunkData chunkData, int axis) {
+    private static bool IsInRange(ChunkData chunkData, int axis) {
         return !(axis < 0 || axis >= chunkData.chunkSize);
     }
 
@@ -25,25 +25,28 @@ public static class Chunk {
         return !(yCoord < 0 || yCoord >= chunkData.chunkHeight);
     }
 
+    private static bool IsVectorInRange(ChunkData chunkData, Vector3Int coords) {
+        return IsInRange(chunkData, coords.x) && InRangeHeight(chunkData, coords.y) && IsInRange(chunkData, coords.z);
+    }
+
     public static BlockType GetBlockFromChunkCoordinates(ChunkData chunkData, Vector3Int chunkCoordinates) {
         return GetBlockFromChunkCoordinates(chunkData, chunkCoordinates.x, chunkCoordinates.y, chunkCoordinates.z);
     }
 
     public static BlockType GetBlockFromChunkCoordinates(ChunkData chunkData, int x, int y, int z) {
-        if (InRange(chunkData, x) && InRangeHeight(chunkData, y) && InRange(chunkData, z)) {
+        if (IsVectorInRange(chunkData, new Vector3Int(x,y,z))) {
             int index = GetIndexFromPosition(chunkData, new Vector3Int(x,y,z));
             return chunkData.blocks[index];
         }
 
-        return chunkData.world.GetBlockFromChunkCoordinates(chunkData, new Vector3Int(chunkData.worldPos.x + x, chunkData.worldPos.y + y, chunkData.worldPos.z + z));
+        Vector3Int coords = new(chunkData.worldPos.x + x, chunkData.worldPos.y + y, chunkData.worldPos.z + z);
+        return chunkData.world.GetBlockFromChunkCoordinates(chunkData, coords);
     }
 
     public static void SetBlock(ChunkData chunkData, Vector3Int localPosition, BlockType block) {
-        if (InRange(chunkData, localPosition.x) && InRangeHeight(chunkData, localPosition.y) && InRange(chunkData, localPosition.z)) {
+        if (IsInRange(chunkData, localPosition.x) && InRangeHeight(chunkData, localPosition.y) && IsInRange(chunkData, localPosition.z)) {
             int index = GetIndexFromPosition(chunkData, localPosition);
             chunkData.blocks[index] = block;
-        } else {
-            throw new Exception("Need to ask World for appropiate chunk");
         }
     }
 
@@ -61,7 +64,8 @@ public static class Chunk {
 
     public static MeshData GetChunkMeshData(ChunkData chunkData) {
         MeshData meshData = new MeshData(true);
-        LoopThroughTheBlocks(chunkData, (x, y, z) => meshData = BlockHelper.GetMeshData(chunkData, new Vector3Int(x, y, z), meshData, chunkData.blocks[GetIndexFromPosition(chunkData, new Vector3Int(x, y, z))]));
+        LoopThroughBlocks(chunkData, (x, y, z) => meshData = BlockHelper.GetMeshData(chunkData, new Vector3Int(x, y, z), meshData, 
+                chunkData.blocks[GetIndexFromPosition(chunkData, new Vector3Int(x, y, z))]));
         return meshData;
     }
 
